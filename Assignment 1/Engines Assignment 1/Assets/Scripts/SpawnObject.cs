69e1b9de5b4016a2c855efb1ba9659ab525bcc47
Christian <christian.ceccarelli@ontariotechu.net>
2019-09-22 13:16:20 -0400

    Updated gitignore and finished spawning objects with level editor

--- Assignment 1/Engines Assignment 1/Assets/Scripts/SpawnObject.cs
+++ Assignment 1/Engines Assignment 1/Assets/Scripts/SpawnObject.cs
@@ -5,12 +5,23 @@ using UnityEngine.UI;
 
 public class SpawnObject : MonoBehaviour
 {
-    public GameObject x;
-    public GameObject y;
-    public GameObject z;
-    
-     public void Spawn()
+    public InputField xPos;
+    public InputField yPos;
+    public InputField zPos;
+
+    public GameObject p;
+
+    float x;
+    float y;
+    float z;
+    public void Spawn()
     {
-        
+         x = float.Parse(xPos.text);
+
+         y = float.Parse(yPos.text);
+
+         z = float.Parse(zPos.text);
+
+        Instantiate(p, new Vector3(x,y,z), Quaternion.identity);
     }
 }
